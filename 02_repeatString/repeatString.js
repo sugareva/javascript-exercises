72ec98422190fa9f415ae4b141163eacc15109c1
Sugareva <43824066+sugareva@users.noreply.github.com>
2023-01-05 21:40:52 +0100

    Exercises 1 and 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+   let i = 0;
+   let array = [];
+   for (i = 0; i < num ; i++) { 
+     array.push(string);
+     }
+     if (num < 0){
+      array.push("ERROR")
+     }
+   return array.join("")
+   }
+   
 // Do not edit below this line
 module.exports = repeatString;
