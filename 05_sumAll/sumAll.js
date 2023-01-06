57672d855f8a2505de91ae88dfb728aebc4a4a30
Sugareva <43824066+sugareva@users.noreply.github.com>
2023-01-06 22:38:43 +0100

    Exercice 5 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let sum = 0;
+    
+    if (first < last){
+        for (i = first; i <= last; i++){
+            sum += i;
+        }
+    
+    } 
+    if(first > last){
+        for (i = last; i <= first; ++i){
+            sum += i;
+        }
+    }
+  if(!Number.isInteger(first) || !Number.isInteger(last) || first < 0 || last < 0 ){
+    sum = "ERROR";
+  }
 
+    return sum;
 };
 
 // Do not edit below this line
